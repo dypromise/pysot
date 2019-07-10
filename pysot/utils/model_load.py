39b5946127940327b10d9fca445354148a5d232b
dypromise <nanhangdingyang@163.com>
2019-07-10 16:39:18 +0800

    developer: fix resume bug

--- pysot/utils/model_load.py
+++ pysot/utils/model_load.py
@@ -40,14 +40,17 @@ def remove_prefix(state_dict, prefix):
     ''' Old style model is stored with all names of parameters
     share common prefix 'module.' '''
     logger.info('remove prefix \'{}\''.format(prefix))
-    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+
+    def f(x):
+        return x.split(prefix, 1)[-1] if x.startswith(prefix) else x
     return {f(key): value for key, value in state_dict.items()}
 
 
 def load_pretrain(model, pretrained_path):
     logger.info('load pretrained model from {}'.format(pretrained_path))
     device = torch.cuda.current_device()
-    pretrained_dict = torch.load(pretrained_path,
+    pretrained_dict = torch.load(
+        pretrained_path,
         map_location=lambda storage, loc: storage.cuda(device))
     if "state_dict" in pretrained_dict.keys():
         pretrained_dict = remove_prefix(pretrained_dict['state_dict'],
@@ -73,7 +76,7 @@ def load_pretrain(model, pretrained_path):
 def restore_from(model, optimizer, ckpt_path):
     device = torch.cuda.current_device()
     ckpt = torch.load(ckpt_path,
-        map_location=lambda storage, loc: storage.cuda(device))
+                      map_location=lambda storage, loc: storage.cuda(device))
     epoch = ckpt['epoch']
 
     ckpt_model_dict = remove_prefix(ckpt['state_dict'], 'module.')
